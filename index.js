79ab200d3ecc8006f4d79dad7c84a7f4a7612216
Facundo Gomez <gomezvera.f@gmail.com>
2021-01-20 10:54:35 +0100

    submitted scramblies solution:

---
+++ index.js
@@ -0,0 +1,21 @@
+// scramblies
+// not my solution, since it timed out
+function scramble(strToBeChecked, strToCheckFor) {
+    let numLetters = {}
+  
+    for (const letter of strToCheckFor) {
+      if (numLetters[letter]) numLetters[letter]++
+      else numLetters[letter] = 1
+    }
+  
+    for (const letter of strToBeChecked) {
+      if (numLetters[letter] && numLetters[letter] !== 0) numLetters[letter]--
+    }
+  
+    for (const key in numLetters) {
+      if (numLetters[key] !== 0) return false
+    }
+  
+    // Only reaches this far if all good
+    return true
+  }
