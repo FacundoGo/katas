c0dbbedaa62b95fcc811992b92751f866916716b
Facundo Gomez <gomezvera.f@gmail.com>
2021-01-25 15:47:48 +0100

    find the unique number

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // scramblies
-// not my solution, since it timed out
+// not my solution, since mine timed out, the below works
 function scramble(strToBeChecked, strToCheckFor) {
     let numLetters = {}
   
@@ -18,4 +18,32 @@ function scramble(strToBeChecked, strToCheckFor) {
   
     // Only reaches this far if all good
     return true
+  }
+
+  // consecutive strings
+
+  function longestConsec(strarr, k) {
+    if (strarr.length == 0 || k > strarr.length || k <= 0) return '';
+    
+    let longStr = '';
+    let newStr = '';
+    
+    // loop over the array, check what is the longest string + K number of consecutive strings
+    for (let i = 0; i < strarr.length; i++){
+      newStr = strarr.slice(i, i+k);
+      if (newStr.join('').length > longStr.length ){
+        longStr = newStr.join('');
+      }
+    }
+    
+    return longStr;
+}
+
+// find the unique number - actually finds all unique values
+// if the first index of a number is equal to the last index of, then it's filtered
+function findUniq(arr) {
+  const filt = arr.filter(el => {
+  return arr.indexOf(el) === arr.lastIndexOf(el)
+  });
+    return filt[0]
   }
